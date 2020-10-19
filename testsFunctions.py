edd490d658772614e6aab7ed56f42807192efd6c
Juliette Thiebaut <57951398+Satanas-sudo@users.noreply.github.com>
2020-10-19 15:07:23 +0200

    Update testsFunctions.py
    More tests added and code corrections

--- testsFunctions.py
+++ testsFunctions.py
@@ -1,76 +1,73 @@
 import unittest
-import main
+import graphReading
 from pathlib import Path
+import time
+
+interaction_file = Path("C:/Users/a/Documents/Cours_rennes1_master_bioinfo/S9_ADG/projet_python/Human_HighQuality.txt")
+file_empty = Path("C:/Users/a/Documents/Cours_rennes1_master_bioinfo/S9_ADG/projet_python/File_Empty.txt")
+first_line_wrong = Path("C:/Users/a/Documents/Cours_rennes1_master_bioinfo/S9_ADG/projet_python/FirstLine_Wrong.txt")
+wrong_num_column = Path("C:/Users/a/Documents/Cours_rennes1_master_bioinfo/S9_ADG/projet_python/Line_WrongColumn.txt")
+
+
+class TimeFunction:
+
+    def time_read_interaction_file(self):
+        """This function measures the time in seconds to execute the functions read_interaction_file_dict and
+        read_interaction_file_list in order to compare their performances."""
+
+        dict_start = time.time()
+        graphReading.read_interaction_file_dict(interaction_file)
+        dict_finished = time.time() - dict_start
+
+        print("Time to execute the function read_interaction_file_dict is {} seconds".format(dict_finished))
+
+
+        list_start = time.time()
+        graphReading.read_interaction_file_list(interaction_file)
+        list_finished = time.time() - list_start
+
+        print("Time to execute the function read_interaction_file_list is {} seconds".format(list_finished))
+
+# Making a list is faster than making a dictionary
 
 
 class TestFunction(unittest.TestCase):
 
     def test_read_interaction_file(self):
         """This function applies several tests to check several features. """
-        interaction_file = Path("C:/Users/a/Documents/Cours_rennes1_master_bioinfo/S9_ADG/projet_python/"
-                                "Human_HighQuality.txt")
 
         """Tests if the output is a dictionary"""
-        dict_type = type(main.read_interaction_file_dict(interaction_file))
-        self.assertEqual(type(dict_type), dict)  # assertEqual() tests the equality between two parameters
+        dict_test = graphReading.read_interaction_file_dict(interaction_file)
+        dict_type = type(dict_test)
+        self.assertEqual(dict_type, dict)  # assertEqual() tests the equality between two parameters
 
         """Tests if the output is a list"""
-        list_type = type(main.read_interaction_file_list(interaction_file))
-        self.assertEqual(type(list_type), list)
+        list_test = graphReading.read_interaction_file_list(interaction_file)
+        list_type = type(list_test)
+        self.assertEqual(list_type, list)
 
         """Tests if the output is a list of tuples"""
-        tuple_type = type(main.read_interaction_file(interaction_file))
-        self.assertEqual(type(tuple_type), tuple)
+        tuple_test = graphReading.read_interaction_file(interaction_file)
+        tuple_type = type(tuple_test)
+        self.assertEqual(tuple_type, tuple)
+
+        """Tests if a file is empty"""
+        first_char = int(file_empty.read(1))  # Gets the first character (if there is one, then it equals to 1)
+        #print(type(first_char))
+        if not first_char:
+            no_char = 0  # If there is not a character in the file, then it equals to 0
+        self.assertEqual(first_char, no_char)
+
+        """Tests if a file whose first line has a wrong number which counts the number of interactions"""
+        first_line = int(first_line_wrong.readline(1))  # Gets the first line
+        lines_num = len(first_line_wrong.readlines())  # Counts the number of lines
+        interaction_int = lines_num - 1
+        self.assertEqual(first_line, interaction_int)
+
+        """Tests if a file has not a first line with the number of interactions"""
+        # TO DO
+
+        """Tests if the number of columns is correct"""
+        # TO DO
 
     unittest.main()
-
-
-"""This function checks if the interaction file is in proper format to be read correctly.
-
-Returns True if correct format.
-1st case : le fichier ne comporte pas la première ligne qui compte le nombre d'interactions
-2nd case : empty file
-3rd case : fichier dont la 1ere ligne contient un nb qui n'est pas le nb d'interactions
-4th case : fichier contenant une ligne qui ne comporte pas le bon nombre de colonne
-5th case : fichier ne contenant que des interlignes"""
-def is_interaction_file(File_Empty):
-
-    with open(File_Empty, "r") as interaction_file:
-        first_char = interaction_file.read(1)  # get the first character
-        if not first_char:
-            print("file is empty")  # first character is the empty string
-        else:
-            interaction_file.seek(0)  # first character wasn't empty, returns to the beginning of the file
-
-is_interaction_file("C:/Users/a/Documents/Cours_rennes1_master_bioinfo/S9_ADG/projet_python/File_Empty.txt")
-
-def is_interaction_file(File_Empty):
-    return os.stat(File_Empty).st_size==0
-    #return os.stat(File_Empty).getsize > 0
-    print("File is empty")
-is_interaction_file("C:/Users/a/Documents/Cours_rennes1_master_bioinfo/S9_ADG/projet_python/File_Empty.txt")
-
-"""def is_interaction_file2(Whitespaces):
-
-    with open(Whitespaces, "r") as interaction_file:
-        content = interaction_file.read()
-        if interaction_file.search(r'^\s*$', content):
-            #^ vérifie position au début de la ligne
-            #\s match avec un espace, revient à [\ r\ n\ t\ f\ v]
-            #* Quantifier — Matches between zero and unlimited times, as many times as possible, giving back as needed (greedy)
-            #$ vérifie la position à la fin de la ligne
-            return True
-    print("file contains only blank lines")
-is_interaction_file2("C:/Users/a/Documents/Cours_rennes1_master_bioinfo/S9_ADG/projet_python/Whitespaces.txt")"""
-
-def is_interaction_file(FirstLine_Wrong):
-
-    with open(FirstLine_Wrong, "r") as interaction_file:
-        first_line = int(interaction_file.readline(1))  # Gets the first line
-        lines_num = count.interaction_file()
-        if first_line == (lines_num-1):
-            print("number of interactions is correct")
-        else:
-            print("number of interactions is false")
-
-is_interaction_file("C:/Users/a/Documents/Cours_rennes1_master_bioinfo/S9_ADG/projet_python/FirstLine_Wrong.txt")
